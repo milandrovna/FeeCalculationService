585ade7046e9719b9d2972c4d987274187aba179
milandrovna <milandrovna@gmail.com>
2025-03-14 17:22:15 +0200

    initial commit

---
+++ src/main/java/com/fujitsu/trialtask/TrialtaskApplication.java
@@ -0,0 +1,13 @@
+package com.fujitsu.trialtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrialtaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrialtaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fujitsu/trialtask/TrialtaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fujitsu.trialtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrialtaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
