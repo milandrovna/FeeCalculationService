7843735e4e117c6a65338ebb96ef0c829c333190
milandrovna <milandrovna@gmail.com>
2025-03-14 19:22:04 +0200

    Added model class for weather condition and enum class with allowed weather phenomenon

---
+++ src/main/java/com/fujitsu/trialtask/enums/WeatherPhenomenon.java
@@ -0,0 +1,24 @@
+package com.fujitsu.trialtask.enums;
+
+public enum WeatherPhenomenon {
+    CLEAR,
+    FEW_CLOUDS,
+    VARIABLE_CLOUDS,
+    CLOUDY_WITH_CLEAR_SPELLS,
+    OVERCAST,
+    LIGHT_SNOW_SHOWER,
+    MODERATE_SNOW_SHOWER,
+    HEAVY_SNOW_SHOWER,
+    LIGHT_RAIN,
+    MODERATE_RAIN,
+    HEAVY_RAIN,
+    GLAZE,
+    LIGHT_SLEET,
+    MODERATE_SLEET,
+    LIGHT_SNOWFALL,
+    MODERATE_SNOWFALL,
+    HEAVY_SNOWFALL,
+    HAIL,
+    MIST,
+    FOG;
+}
---
+++ src/main/java/com/fujitsu/trialtask/model/WeatherCondition.java
@@ -0,0 +1,38 @@
+package com.fujitsu.trialtask.model;
+
+import com.fujitsu.trialtask.enums.WeatherPhenomenon;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotNull;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class WeatherCondition {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @NotNull
+    private Integer wmoStationCode;
+
+    @NotNull
+    private Float airTemperature;
+
+    @NotNull
+    private Float windSpeed;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private WeatherPhenomenon weatherPhenomenon;
+
+    @NotNull
+    private Long timestamp;
+
+}
