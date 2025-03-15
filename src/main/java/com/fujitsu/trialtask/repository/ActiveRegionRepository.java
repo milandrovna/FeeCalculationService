d010d0383943b6f1e586edba6767e8de1df40104
milandrovna <milandrovna@gmail.com>
2025-03-15 17:53:25 +0200

    Added repositories

---
+++ src/main/java/com/fujitsu/trialtask/repository/ActiveRegionRepository.java
@@ -0,0 +1,8 @@
+package com.fujitsu.trialtask.repository;
+
+import com.fujitsu.trialtask.model.ActiveRegion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActiveRegionRepository extends JpaRepository<ActiveRegion, Integer> {
+
+}
---
+++ src/main/java/com/fujitsu/trialtask/repository/WeatherConditionRepository.java
@@ -0,0 +1,8 @@
+package com.fujitsu.trialtask.repository;
+
+import com.fujitsu.trialtask.model.WeatherCondition;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WeatherConditionRepository extends JpaRepository<WeatherCondition, Integer> {
+
+}
