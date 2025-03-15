d4cb3000dbbe0810843c49c7babd586fd41ca0b9
milandrovna <milandrovna@gmail.com>
2025-03-15 14:45:40 +0200

    added configuration files

--- src/main/java/com/fujitsu/trialtask/config/ApplicationConfig.java
+++ src/main/java/com/fujitsu/trialtask/config/ApplicationConfig.java
@@ -1,11 +1,9 @@
 package com.fujitsu.trialtask.config;
 
-import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableAsync
-@EnableWebMvc
+@Configuration
 @EnableScheduling
 public class ApplicationConfig {
 }
